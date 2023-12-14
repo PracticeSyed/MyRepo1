8d76446f7989e363e0cbd876d208d5bf72e90811
PracticeSyed <syed16riyaz@gmail.com>
2023-12-14 16:29:00 +0530

    Added few lines of code

---
+++ src/org/sample/SampleClass.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class SampleClass {
+	
+	public static void sample() {
+		
+		System.out.println("Start");
+	}
+
+	public static void main(String[] args) {
+		
+		sample();
+	}
+	
+}
